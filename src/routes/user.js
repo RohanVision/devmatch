8491834b035434ecd70a6e2fda0763550a325283
Rohan Amberkar <amberkar.rohan17@gmail.com>
2025-02-16 17:43:03 -0500

    Feed API done

--- src/routes/user.js
+++ src/routes/user.js
@@ -56,6 +56,10 @@ userRouter.get("/user/connection", userAuth, async (req, res) => {
 userRouter.get("/feed", userAuth, async (req, res) => {
     try {
         loggedInUser = req.user;
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page - 1) * limit;
 
         const connectionRequest = await ConnectionRequest.find({
             $or: [
@@ -79,7 +83,7 @@ userRouter.get("/feed", userAuth, async (req, res) => {
                     _id: { $ne: loggedInUser._id },
                 }
             ]
-        }).select(USER_SAFE_DATA)
+        }).select(USER_SAFE_DATA).skip(skip).limit(limit)
 
         res.send(users)
 
