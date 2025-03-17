16202755a0dfc03c2e6c2b0d330d4e3aea1b5d9d
Rohan Amberkar <amberkar.rohan17@gmail.com>
2025-03-17 16:44:44 -0400

    Updated DB Password

--- src/config/database.js
+++ src/config/database.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 // To connect our Clusture. 
 const connectDB = async () => {
     // Also it return promise so we need to call inside Async function
-    await mongoose.connect('mongodb+srv://amberkarrohan17:wildfire123@cluster0.la9dial.mongodb.net/devmatch');
-    // mongodb+srv://amberkarrohan17:wildfire123@cluster0.la9dial.mongodb.net/helloWordl - connect to Specific database
+    await mongoose.connect('mongodb+srv://amberkarrohan17:oamM5rqQ0FEPIrkW@cluster0.la9dial.mongodb.net/devmatch');
+    // mongodb+srv://amberkarrohan17:oamM5rqQ0FEPIrkW@cluster0.la9dial.mongodb.net/helloWordl - connect to Specific database
 }
 
 module.exports = connectDB;
